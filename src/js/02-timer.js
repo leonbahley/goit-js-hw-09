84899cf0a7035bb667f966d171e2e9bb0f09c7fe
leonbahley <leon.bahley@icloud.com>
2022-10-18 17:00:46 +0200

    hw9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -20,7 +20,7 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] <= Date.now()) {
-      alert('choose another date');
+      alert('Please choose a date in the future');
       return;
     }
     selectedDate = selectedDates[0];
@@ -31,12 +31,13 @@ const options = {
 const timer = {
   intervalId: null,
   startTimer() {
+    refs.startBtn.setAttribute('disabled', '');
     this.intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = selectedDate - currentTime;
       const timeComponents = convertMs(deltaTime);
       this.updateCurrentValues(timeComponents);
-      if (deltaTime === 0) {
+      if (deltaTime < 1000) {
         this.stopTimer();
       }
     }, 1000);
@@ -57,18 +58,20 @@ function convertMs(ms) {
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-  const days = pad(Math.floor(ms / day));
-  const hours = pad(Math.floor((ms % day) / hour));
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
 
-function pad(value) {
+function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
-refs.startBtn.addEventListener('click', timer.startTimer());
+refs.startBtn.addEventListener('click', timer.startTimer.bind(timer));
 
 flatpickr(refs.selectDate, options);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+refs.form.addEventListener('input', onInput);
+let delay = null;
+let amount = null;
+let step = null;
+
+function onInput(event) {
+  delay = Number(refs.delay.value);
+  amount = Number(refs.amount.value);
+  step = Number(refs.step.value);
+  console.log(delay);
+  console.log(amount);
+  console.log(step);
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    delay += step;
+  }
+
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    });
   }
 }
